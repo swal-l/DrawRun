434d55a5631b410969729e38e6125652b4a07119
Lomic <lomic@example.com>
2026-01-10 20:17:53 +0100

    🚀 Deploy v3.41

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.orbital.run"
         minSdk = 26
         targetSdk = 35
-        versionCode = 85
-        versionName = "3.40"
+        versionCode = 86
+        versionName = "3.41"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
@@ -141,6 +141,7 @@ dependencies {
 tasks.register("testClasses") {
     dependsOn("compileDebugUnitTestSources")
 }
+
 
 
 
